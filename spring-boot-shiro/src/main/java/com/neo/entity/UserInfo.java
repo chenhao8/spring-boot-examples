e1c0cacb17af14b694eab4bd17a22a6e9b7336a0
chenhao <chenhao@namibank.com>
2017-06-27 15:51:18 +0800

    clean is

--- spring-boot-shiro/src/main/java/com/neo/entity/UserInfo.java
+++ spring-boot-shiro/src/main/java/com/neo/entity/UserInfo.java
@@ -1,86 +1,98 @@
 package com.neo.entity;
 
-import javax.persistence.*;
 import java.io.Serializable;
 import java.util.List;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
 @Entity
 public class UserInfo implements Serializable {
-    @Id
-    @GeneratedValue
-    private Integer uid;
-    @Column(unique =true)
-    private String username;//帐号
-    private String name;//名称（昵称或者真实姓名，不同系统不同定义）
-    private String password; //密码;
-    private String salt;//加密密码的盐
-    private byte state;//用户状态,0:创建未认证（比如没有激活，没有输入验证码等等）--等待验证的用户 , 1:正常状态,2：用户被锁定.
-    @ManyToMany(fetch= FetchType.EAGER)//立即从数据库中进行加载数据;
-    @JoinTable(name = "SysUserRole", joinColumns = { @JoinColumn(name = "uid") }, inverseJoinColumns ={@JoinColumn(name = "roleId") })
-    private List<SysRole> roleList;// 一个用户具有多个角色
 
-    public Integer getUid() {
-        return uid;
-    }
+	private static final long serialVersionUID = -6441997020641122861L;
 
-    public void setUid(Integer uid) {
-        this.uid = uid;
-    }
+	@Id
+	@GeneratedValue
+	private Integer uid;
+	@Column(unique = true)
+	private String username;// 帐号
+	private String name;// 名称（昵称或者真实姓名，不同系统不同定义）
+	private String password; // 密码;
+	private String salt;// 加密密码的盐
+	private byte state;// 用户状态,0:创建未认证（比如没有激活，没有输入验证码等等）--等待验证的用户 , 1:正常状态,2：用户被锁定.
+	@ManyToMany(fetch = FetchType.EAGER)
+	// 立即从数据库中进行加载数据;
+	@JoinTable(name = "SysUserRole", joinColumns = { @JoinColumn(name = "uid") }, inverseJoinColumns = { @JoinColumn(name = "roleId") })
+	private List<SysRole> roleList;// 一个用户具有多个角色
 
-    public String getUsername() {
-        return username;
-    }
+	public Integer getUid() {
+		return uid;
+	}
 
-    public void setUsername(String username) {
-        this.username = username;
-    }
+	public void setUid(Integer uid) {
+		this.uid = uid;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getUsername() {
+		return username;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setUsername(String username) {
+		this.username = username;
+	}
 
-    public String getPassword() {
-        return password;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public String getSalt() {
-        return salt;
-    }
+	public String getPassword() {
+		return password;
+	}
 
-    public void setSalt(String salt) {
-        this.salt = salt;
-    }
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
-    public byte getState() {
-        return state;
-    }
+	public String getSalt() {
+		return salt;
+	}
 
-    public void setState(byte state) {
-        this.state = state;
-    }
+	public void setSalt(String salt) {
+		this.salt = salt;
+	}
 
-    public List<SysRole> getRoleList() {
-        return roleList;
-    }
+	public byte getState() {
+		return state;
+	}
 
-    public void setRoleList(List<SysRole> roleList) {
-        this.roleList = roleList;
-    }
+	public void setState(byte state) {
+		this.state = state;
+	}
 
-    /**
-     * 密码盐.
-     * @return
-     */
-    public String getCredentialsSalt(){
-        return this.username+this.salt;
-    }
-    //重新对盐重新进行了定义，用户名+salt，这样就更加不容易被破解
+	public List<SysRole> getRoleList() {
+		return roleList;
+	}
+
+	public void setRoleList(List<SysRole> roleList) {
+		this.roleList = roleList;
+	}
+
+	/**
+	 * 密码盐.
+	 * @return
+	 */
+	public String getCredentialsSalt() {
+		return this.username + this.salt;
+	}
+	// 重新对盐重新进行了定义，用户名+salt，这样就更加不容易被破解
 }
